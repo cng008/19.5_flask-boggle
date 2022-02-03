76a66b770db0cf5fa11688e91577706c9dd7e8f1
Christien Ng <ngchristien@gmail.com>
2022-02-02 17:24:00 -0800

    display board, add styling

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from flask import Flask, request, render_template, session
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'shhhh'
+# debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 boggle_game = Boggle()
+
+@app.route("/")
+def index():
+    """ show game board """
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    
+    return render_template("index.html", board=board)
