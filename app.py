79acc03901e9d029a8f3822953d8af6a08482f1c
Christien Ng <ngchristien@gmail.com>
2022-02-03 20:31:29 -0800

    check for valid word using AJAX

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
-from flask import Flask, request, render_template, session
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, session, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'shhhh'
-# debug = DebugToolbarExtension(app)
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 
 @app.route("/")
 def index():
-    """ show game board """
+    """ Show game board """
 
     board = boggle_game.make_board()
     session['board'] = board
     
     return render_template("index.html", board=board)
 
-@app.route("/check", methods=["POST"])
+
+@app.route("/check")
 def check_word():
-    """ Save response and redirect to next question """
+    """ Checks if word is in words.txt """
 
-    # get text from form
-    guess = request.form['guess']
+    word = request.args['word']
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
 
-    # add response to session
-    response = session['board']
-    response.append(guess)
-    session['board'] = response
-
-    if (len(response) == len(survey.questions)):
-        # They've answered all the questions! Thank them.
-        return redirect("/thanks")
-
-    else:
-        return redirect(f"/questions/{len(response)}")
+    return jsonify({'result': response})
--- static/boggle.js
+++ static/boggle.js
@@ -1,42 +1,86 @@
-/** HANDLE GAME CLICK */
+let score = 0;
+let countdown = 60;
+let words = new Set();
+
+/* HANDLE BOARD CLICK */
 // Add letter to input on click
 $('span').on('click', e => {
-  // e.preventDefault();
   let letter = $(e.target).text();
-  $('#guess').val($('#guess').val() + letter);
-  // console.log(letter);
+  $('#word').val($('#word').val() + letter);
 });
 
 /** GAME TIMER
  * at 0, stop timer and don't allow any more game clicks
  */
-let countdown = 60;
 $('#timer').text(countdown);
 let interval = setInterval(function () {
   countdown = countdown - 1;
   $('#timer').text(countdown);
-  if (countdown == 10) {
+  if (countdown === 10) {
     $('#timer').css('color', 'orange');
   }
-  if (countdown == 0) {
+  if (countdown === 0) {
     clearInterval(interval);
     $('#timer').text("time's up!");
     // disable board clicks, text input, check button
     $('span').off();
-    $('#guess').attr('disabled', 'disabled');
+    $('#word').attr('disabled', 'disabled');
     $('#check').prop('disabled', true);
   }
 }, 1000);
 
+/* ADD WORD TO LIST OF WORDS */
+showWord = word => {
+  $('.words').append($('<li>', { text: word }));
+  // $('.words').text($('.words').text() + word + ', ');
+};
+
 /** GAME SCORE COUNTER
  * score for a word is equal to its length
+ * show score in html
  */
-let counter = 0;
-// set default score
-$('#score').text('0');
+$('#score').text('0'); // set default score
+showScore = () => {
+  $('#score').text(score);
+};
 
-// validate word
-// add num of letters to score
+/* STATUS MESSAGE */
+showMessage = (msg, cls) => {
+  $('.msg').text(msg).removeClass().addClass(`msg ${cls}`);
+};
 
-// update current score
-$('#score').text(counter);
+/** HANDLE SUBMISSION OF WORDS
+ * if unique and valid, score & show
+ */
+$('#add-word').on('submit', handleSubmit);
+async function handleSubmit(e) {
+  e.preventDefault();
+  const $word = $('#word');
+
+  let word = $word.val().toUpperCase();
+  if (!word) return;
+
+  if (words.has(word)) {
+    showMessage(`Already found ${word}!`);
+    $word.val('');
+    return;
+  }
+
+  // Validation check
+  const resp = await axios.get('/check', {
+    params: { word: word.toLowerCase() }
+  });
+  if (resp.data.result === 'not-word') {
+    showMessage(`${word} is not a valid English word`, 'err');
+  } else if (resp.data.result === 'not-on-board') {
+    showMessage(`${word} is not a valid word on this board`, 'err');
+  } else {
+    showWord(word);
+    score += word.length;
+    showScore();
+    words.add(word);
+    showMessage(`Added: ${word}`, 'ok');
+  }
+
+  $word.val('').focus();
+}
